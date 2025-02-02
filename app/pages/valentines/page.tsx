ee1fcdebe73fa17581975e062fcaa0ccc2682fee
Gathrean Dela Cruz <gathrean@icloud.com>
2025-02-01 20:37:13 -0800

    Added Valentine's page (empty)

---
+++ app/pages/valentines/page.tsx
@@ -0,0 +1,12 @@
+// app/valentines/page.tsx
+'use client';
+
+export default function Valentines() {
+    return (
+        <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-forest_night">
+            <main className="flex-1 flex flex-col items-center justify-center p-8">
+                {/* Blank page, no content here */}
+            </main>
+        </div>
+    );
+}
