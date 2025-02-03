d8e9bb10384f68ddd94c425e24aa7b0c63348ace
Gathrean Dela Cruz <gathrean@icloud.com>
2025-02-02 22:03:33 -0800

    Forgot to push this

--- app/pages/valentines/1-no/page.tsx
+++ app/pages/valentines/1-no/page.tsx
@@ -11,7 +11,7 @@ export default function Nope() {
             <h1 className="text-4xl font-bold text-gray-300">
                 i see...
             </h1>
-            <img src="/Valentines/spiderman.jpeg" alt="Sad cat gif" className="mt-4" />
+            <img src="/Valentines/1/spiderman.jpeg" alt="Sad cat gif" className="mt-4" />
             <button
                 onClick={() => router.push('/pages/valentines')}
                 className="mt-4 bg-gray-800 text-white p-4 rounded-full w-60 hover:bg-gray-900"
