f9084602c94941b333e911538387c4908301abbe
Gathrean Dela Cruz <gathrean@icloud.com>
2025-02-02 21:16:06 -0800

    Added styling for 1-no page

--- app/pages/valentines/1-no/page.tsx
+++ app/pages/valentines/1-no/page.tsx
@@ -1,20 +1,22 @@
 'use client';
 import { useRouter } from 'next/navigation';
 
-import '../valentines.css'; 
+import '../valentines.css';
 
 export default function Nope() {
     const router = useRouter();
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen p-8">
-            <h1 className="text-4xl font-bold text-gray-800">I know you're kidding baby but that hurt a little bit :(</h1>
-            <img src="/Valentines/sad-cat.gif" alt="Sad cat gif" className="mt-4" />
+        <div className="flex flex-col items-center justify-center min-h-screen p-8 bg-black text-gray-200">
+            <h1 className="text-4xl font-bold text-gray-300">
+                i see...
+            </h1>
+            <img src="/Valentines/spiderman.jpeg" alt="Sad cat gif" className="mt-4" />
             <button
                 onClick={() => router.push('/pages/valentines')}
-                className="mt-4 bg-yellow-500 text-white p-4 rounded-full w-40 hover:bg-yellow-600"
+                className="mt-4 bg-gray-800 text-white p-4 rounded-full w-60 hover:bg-gray-900"
             >
-                I changed my mind!
+                WAIT! TAKE ME BACK!
             </button>
         </div>
     );
