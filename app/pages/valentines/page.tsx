25a36574beec5c0d4ff7d529b7afcbf9a1efc3a4
Gathrean Dela Cruz <gathrean@icloud.com>
2025-02-03 23:45:24 -0800

    Added extra text

--- app/pages/valentines/page.tsx
+++ app/pages/valentines/page.tsx
@@ -25,21 +25,13 @@ export default function Valentines() {
     return (
         <div className="valentines-page">
             <main className="flex-1 flex flex-col items-center justify-center p-8">
-                <h1 className="text-5xl font-bold">Will you be my Valentine?</h1>
+                <h1 className="text-5xl font-bold">WILL YOU BE MY VALENTINE?</h1>
                 <p className="text-4xl mt-2">🐢❤️🐻‍❄️</p>
+                <p>(V-<b>A-L-O-E</b>-N-T-<b>E-A-N-</b>E)</p>
                 <br />
                 <img src="/Valentines/cat-begging.gif" alt="Begging cat" className="w-120 h-120" />
                 <br />
                 <div className="mt-8 flex gap-4">
-                    <button
-                        onClick={() => {
-                            submitResponse('Yes, I will be your Valentine!');
-                            router.push('/pages/valentines/1-yes');
-                        }}
-                        className="bg-red-500 text-white p-4 rounded-full w-48 hover:bg-red-600"
-                    >
-                        YESSS!!!!!!!!!
-                    </button>
                     <button
                         onClick={() => {
                             submitResponse('No, I won\'t be your Valentine :(');
@@ -48,6 +40,15 @@ export default function Valentines() {
                         className="bg-gray-500 text-white p-4 rounded-full w-32 hover:bg-gray-600"
                     >
                         nope...
+                    </button>
+                    <button
+                        onClick={() => {
+                            submitResponse('Yes, I will be your Valentine!');
+                            router.push('/pages/valentines/1-yes');
+                        }}
+                        className="bg-red-500 text-white p-4 rounded-full w-48 hover:bg-red-600"
+                    >
+                        YESSS!!!!!!!!!
                     </button>
                 </div>
             </main>
